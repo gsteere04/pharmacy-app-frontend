c6664b529ddfe26b43658ce4bbf213bf30277f8d
Grant <gsteere4717@xmail.dixietech.edu>
2024-08-07 11:19:31 -0600

    create and add simple navbar

--- pharmacy-frontend/src/App.tsx
+++ pharmacy-frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css'
+import NavBar from "./components/NavBar";
 import HomePage from './pages/HomePage'
 import Prescription from "./pages/Prescription";
 
@@ -9,6 +10,7 @@ const App: React.FC = () => {
   return (
     <Router>
       <div className="App">
+        <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />}/>
           <Route path="/prescription" element={<Prescription />}/>
---
+++ pharmacy-frontend/src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar: React.FC = () => {
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/prescription">Presciptions</Link></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
