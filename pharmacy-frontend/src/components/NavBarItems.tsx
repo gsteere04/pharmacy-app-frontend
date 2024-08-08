d2d0ae85a5bd39e41aaa183846665eff19f9ad6f
Grant <gsteere4717@xmail.dixietech.edu>
2024-08-08 11:08:17 -0600

    adjust logos and navbar

--- pharmacy-frontend/src/components/NavBarItems.tsx
+++ pharmacy-frontend/src/components/NavBarItems.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './NavBarItems.css';
-import logo from '../assets/image.png'
-
+import logo from '../assets/image.png';
+import logo2 from '../assets/rx-icon-25472.png';
 const NavBarItems: React.FC = () => {
     return (
         <nav className="navbaritems">
+            <div className="logos">
             <img src={logo} alt="Logo" className="navbar-logo" />
+            <img src={logo2} alt="Logo2" className="navbar-rxlogo" /> 
+            </div>
             <ul className="nav-list">
                 <li className="nav-item"><Link to="/">Home</Link></li>
                 <li className="nav-item"><Link to="/prescription">Presciptions</Link></li>
