1db2a9d60d5b84ba982451e29584972d735388f3
Grant <gsteere4717@xmail.dixietech.edu>
2024-08-12 11:06:42 -0600

    start patients page, add styles and add to page

---
+++ pharmacy-frontend/src/components/PrescriptionStatus/PrescriptionStatus.tsx
@@ -0,0 +1,13 @@
+//import React, { useState, useEffect } from "react";
+//import './PrescriptionStatus.css';
+
+//const PresciptionStatus: React.FC = () => {
+//    
+//    const [prescriptions, setPrescriptions] = useState<Array<{ id: number, name: string, status: string }>>([]);
+//
+//    useEffect(() => {
+
+//    });
+//};
+
+//export default PresciptionStatus;
--- pharmacy-frontend/src/pages/Patients Page/Patients.tsx
+++ pharmacy-frontend/src/pages/Patients Page/Patients.tsx
@@ -2,12 +2,27 @@ import React from "react";
 import "./Patients.css";
 
 const Patients: React.FC = () => {
-    return (
-        <div className="patients">
-            <h1>Patients</h1>
-            <p>Start of Patients Page</p>
+  return (
+    <div className="patients">
+      <div className="patient-name-card">
+        <h1 className="patient-name">Patient Name: --- Steere, Grant </h1>
+      </div>
+      <div className="patient-container">
+        <h1 className="patient-title">Patient Information</h1>
+        <div className="patient-card">
+          <div className="patient-card-text">
+            <p>Address:</p>
+            <p>Phone:</p>
+            <p>DOB:</p>
+          </div>
         </div>
-    );
+        <div className="allergies-balance">
+        <h2>Allergies: --</h2>
+        <h2>Balance: --</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Patients;
