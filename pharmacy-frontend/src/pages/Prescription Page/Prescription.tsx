7f7b4609f04857dd40f0789c0168210691b58085
Grant <gsteere4717@xmail.dixietech.edu>
2024-08-15 09:16:44 -0600

    update prescription page, add prescription details and styling

--- pharmacy-frontend/src/pages/Prescription Page/Prescription.tsx
+++ pharmacy-frontend/src/pages/Prescription Page/Prescription.tsx
@@ -2,13 +2,33 @@ import React from "react";
 import "./Prescription.css";
 
 const Prescription: React.FC = () => {
-    return (
-        <div className="prescription">
-            <div className="prescription-action">
-                <h2>New/Refill Prescription -- Sample/Grant (Rx Number: ---)</h2>
-            </div>
-        </div>
-    );
+  return (
+    <div className="prescription">
+      <div className="prescription-action">
+        <h2>New/Refill Prescription -- Sample/Grant (Rx Number: ---)</h2>
+      </div>
+      <div className="prescription-details">
+        <form>
+          <label>
+            Patient:
+            <input type="text" name="patient" />
+          </label>
+        </form>
+        <form>
+          <label>
+            Prescriber:
+            <input type="text" name="prescriber" />
+          </label>
+        </form>
+        <form>
+          <label>
+            Item:
+            <input type="text" name="item" />
+          </label>
+        </form>
+      </div>
+    </div>
+  );
 };
 
-export default Prescription
+export default Prescription;
