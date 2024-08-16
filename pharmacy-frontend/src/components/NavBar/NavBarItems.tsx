77a08b7510a7b5ffcacb7f6474e9ee23f9717010
Grant <gsteere4717@xmail.dixietech.edu>
2024-08-16 08:55:02 -0600

    adjust styling, remove home from navbar, make logo home page button.

--- pharmacy-frontend/src/components/NavBar/NavBarItems.tsx
+++ pharmacy-frontend/src/components/NavBar/NavBarItems.tsx
@@ -2,18 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./NavBarItems.css";
 import logo from "/src/assets/image.png";
-import logo2 from "/src/assets/rx-icon-25472.png";
 const NavBarItems: React.FC = () => {
   return (
     <nav className="navbaritems">
       <div className="logos">
-        <img src={logo} alt="Logo" className="navbar-logo" />
-        <img src={logo2} alt="Logo2" className="navbar-rxlogo" />
+        <a href="/">
+         <img src={logo} alt="Logo" className="navbar-logo" />
+        </a>
       </div>
       <ul className="nav-list">
-        <li className="nav-item">
-          <Link to="/">Home</Link>
-        </li>
         <li className="nav-item">
           <Link to="/prescription">Presciptions</Link>
         </li>
@@ -25,6 +22,9 @@ const NavBarItems: React.FC = () => {
         </li>
         <li className="nav-item">
           <Link to="/medication">Medicine</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/login">Login</Link>
         </li>
       </ul>
     </nav>
--- pharmacy-frontend/src/pages/Prescription Page/Prescription.tsx
+++ pharmacy-frontend/src/pages/Prescription Page/Prescription.tsx
@@ -2,30 +2,59 @@ import React from "react";
 import "./Prescription.css";
 
 const Prescription: React.FC = () => {
+
+    //const emptyPrescription = 
+    //const filled Prescription = 
+    
+
   return (
     <div className="prescription">
       <div className="prescription-action">
-        <h2>New/Refill Prescription -- Sample/Grant (Rx Number: ---)</h2>
+        <h3>New/Refill Prescription -- {} (Rx Number: ---)</h3>
       </div>
-      <div className="prescription-details">
-        <form>
-          <label>
-            Patient:
-            <input type="text" name="patient" />
-          </label>
-        </form>
-        <form>
-          <label>
-            Prescriber:
-            <input type="text" name="prescriber" />
-          </label>
-        </form>
-        <form>
-          <label>
-            Item:
-            <input type="text" name="item" />
-          </label>
-        </form>
+      <div className="cards">
+        <div className="prescription-details">
+          <form>
+            <label>
+              Patient:
+              <input type="text" name="patient" placeholder="Ex. John Doe" />
+            </label>
+          </form>
+          <form>
+            <label>
+              Prescriber:
+              <input type="text" name="prescriber" placeholder="Ex. John Cena" />
+            </label>
+          </form>
+          <form>
+            <label>
+              Item:
+              <input type="text" name="item" placeholder="Ex. Motrin" />
+            </label>
+          </form>
+        </div>
+
+        <div className="patient-card">
+            <h3>Patient Information</h3>
+          <form>
+            <label>
+              Adress:
+              <input type="text" name="adress" placeholder="Ex. 296 The Moon"/>
+            </label>
+          </form>
+          <form>
+            <label>
+              Phone:
+              <input type="text" name="phone" placeholder="Ex. 435-1000-0000" />
+            </label>
+          </form>
+          <form>
+            <label>
+              DOB:
+              <input type="text" name="dob" placeholder="Ex. 8/15/1864" />
+            </label>
+          </form>
+        </div>
       </div>
     </div>
   );
